77cf204ac4eb8c7ba6ca116b6f049bd86f2c3f7e
Saira Bibi <sairabibi248@gmail.com>
2026-09-01 17:34:56 +0500

    Basic Calculator App

--- script.js
+++ script.js
@@ -1,56 +1,21 @@
-let screen = document.querySelector(".calculator-screen input");
-let btns = document.querySelectorAll(".buttons button");
+const screen = document.querySelector(".calculator-screen input");
+const btns = document.querySelectorAll(".buttons button");
 
-btns[0].onclick = function () {
-  screen.value = screen.value + "1";
-};
-btns[1].onclick = function () {
-  screen.value = screen.value + "2";
-};
-btns[2].onclick = function () {
-  screen.value = screen.value + "3";
-};
-btns[3].onclick = function () {
-  screen.value = screen.value + "/";
-};
-btns[4].onclick = function () {
-  screen.value = screen.value + "4";
-};
-btns[5].onclick = function () {
-  screen.value = screen.value + "5";
-};
-btns[6].onclick = function () {
-  screen.value = screen.value + "6";
-};
-btns[7].onclick = function () {
-  screen.value = screen.value + "+";
-};
-btns[8].onclick = function () {
-  screen.value = screen.value + "7";
-};
-btns[9].onclick = function () {
-  screen.value = screen.value + "8";
-};
-btns[10].onclick = function () {
-  screen.value = screen.value + "9";
-};
-btns[11].onclick = function () {
-  screen.value = screen.value + "-";
-};
-btns[12].onclick = function () {
-  screen.value = screen.value + "0";
-};
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function () {
+    const value = btn.dataset.value;
+    const action = btn.dataset.action;
 
-btns[13].onclick = function () {
-  try {
-    screen.value = eval(screen.value);
-  } catch (error) {
-    screen.value = "Error";
-  }
-};
-btns[14].onclick = function () {
-  screen.value = screen.value + "*";
-};
-btns[15].onclick = function () {
-  screen.value = "";
-};
+    if (action === "clear") {
+      screen.value = "";
+    } else if (action === "calculate") {
+      try {
+        screen.value = eval(screen.value);
+      } catch {
+        screen.value = "Error";
+      }
+    } else {
+      screen.value += value;
+    }
+  });
+});
