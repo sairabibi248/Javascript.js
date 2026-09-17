7dea831b78602d0ec10b19dfb826b4b0a505f431
Saira Bibi <sairabibi248@gmail.com>
2026-09-17 17:24:18 +0500

    feat: add modular structure

--- Calcuverse/js/scientificCalculator.js
+++ Calcuverse/js/scientificCalculator.js
@@ -6,7 +6,7 @@ export function initScientificCalculator() {
     '[data-content="scientific-calculator"] .buttons button',
   );
 
-  let isDegree = true; // Default Mode: Degree (DEG)
+  let isDegree = true; 
 
   function getFactorial(n) {
     if (n < 0) return "Error";
@@ -31,18 +31,18 @@ export function initScientificCalculator() {
         let num = Number(sciScreen.value);
         sciScreen.value = getFactorial(num);
       } else if (action === "toggle") {
-        // DEG aur RAD toggle logic
+       
         isDegree = !isDegree;
 
-        // Button text change ho kar dikhayega ke kaun sa mode active hai
+        
         btn.textContent = isDegree ? "DEG" : "RAD";
       } else if (action === "calculate") {
         try {
           let expression = sciScreen.value;
 
-          // Trigonometric Functions
+        
           if (isDegree) {
-            // Degree Mode: Angle ko (Math.PI / 180) se multiply karein
+       
             expression = expression.replaceAll(
               "sin(",
               "Math.sin((Math.PI/180)*",
@@ -56,7 +56,7 @@ export function initScientificCalculator() {
               "Math.tan((Math.PI/180)*",
             );
           } else {
-            // Radian Mode: Direct Math functions
+          
             expression = expression.replaceAll("sin(", "Math.sin(");
             expression = expression.replaceAll("cos(", "Math.cos(");
             expression = expression.replaceAll("tan(", "Math.tan(");
